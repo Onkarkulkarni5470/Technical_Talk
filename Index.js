b91ac7e32baa00b6d6ddd60f1c910f780016fe78
Onkarkulkarni5470 <kulkarnio5470@gmail.com>
2024-08-21 00:24:01 +0530

    index js

--- Index.js
+++ Index.js
@@ -1,22 +1,45 @@
 import express from "express";
-
-import {newuser} from "./routes/loginpage/loginAndsignup.js"
-import  {auth} from "./routes/loginpage/login.js"
-import  {users} from "./routes/provideuserinfo/userinfo.js"
-import  {corsMiddleware} from "./Middleware/handelcors.js"
+import http from "http";
+import { Server } from 'socket.io';
+import { newuser } from "./routes/loginpage/loginAndsignup.js";
+import { auth } from "./routes/loginpage/login.js";
+import { users } from "./routes/provideuserinfo/userinfo.js";
+import { corsMiddleware } from "./Middleware/handelcors.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",  
+        methods: ["GET", "POST"]
+    }
+});
+
 app.use(corsMiddleware);
 app.options('*', corsMiddleware);
 
 app.use(express.json());
 
-app.use("/",newuser);
-app.use("/",auth);
-app.use("/",users)
+app.use("/", newuser);
+app.use("/", auth);
+app.use("/", users);
 
+const PORT = 8080;
 
-const PORT=8080;
-app.listen(PORT ,()=>{
-   console.log(`the port is lisening ${PORT}`) 
-})
+io.on('connection', (socket) => {
+    console.log("New user connected");
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);  
+    });
+
+   
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
